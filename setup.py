90cd1396ec8010ae466fbfb67d71f3e2ed79c6b2
shirans <shoshanshiran@gmail.com>
2022-02-22 11:17:34 -0500

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,34 @@
+import setuptools
+import yaml
+
+name = 'qrm_client'
+file_path = 'qrm_client'
+
+with open(f'{file_path}/requirements.txt') as fid:
+    install_requires = fid.readlines()
+
+
+def get_version_info() -> str:
+    try:
+        with open(f'{file_path}/ver.yaml') as fid:
+            version = yaml.safe_load(fid).get('version')
+            print(version)
+    except FileNotFoundError:
+        with open(f'{file_path}/_ver.yaml') as fid:
+            version = yaml.safe_load(fid).get('version')
+            print(version)
+
+    return str(version)
+
+
+setuptools.setup(
+    name=name,
+    version=get_version_info(),
+    description=name,
+    url='https://github.com/final-israel/qrm',
+    author='iPerf',
+    author_email='pavelr@final.israel',
+    install_requires=install_requires,
+    python_requires='>=3.6.9',
+    packages=['qrm_defs', 'qrm_client']
+)
